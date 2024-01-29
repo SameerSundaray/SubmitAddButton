8a34c438819369c77b2485650c919dbc1e655349
SameerSundaray <sameersundaray2014@gmail.com>
2024-01-29 11:07:29 +0530

    Your commit message

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,24 +7,19 @@ import './ExpenseItem.css';
 const ExpenseItem = (props) => {
 
   const  [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount);
   const clickHandler = () => {
     setTitle('Updated!');
     console.log(title);
   };
-  const clickAmountHandler = () => {
-    setAmount('100');
-    console.log(amount);
-  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item__price'>${amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
      <button onClick ={clickHandler}>Change TItle</button>
-     <button onClick ={clickAmountHandler}>Change Amount</button>
     </Card>
   );
 }
